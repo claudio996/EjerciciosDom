c9818ce4f1054ff203afabbdbe6d571073c19727
claudio996 <claudioantonio.retamal@gmail.com>
2021-11-08 22:39:10 -0300

    estructura jtml

---
+++ js/main.js
@@ -0,0 +1,12 @@
+//import {saludo}  from './module.js' 
+//saludo();
+/*
+const $nav =  document.createElement("nav");
+const $title = document.createElement("h1");
+const $html =  document.documentElement;
+
+let yellow = getComputedStyle($html).getPropertyValue("--yellow-color")
+
+$nav.style.backgroundColor(yellow);
+console.log($nav);
+*/
