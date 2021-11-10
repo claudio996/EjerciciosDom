aef277654a9a382e6eaebbc544141afa725c0f91
claudio996 <claudioantonio.retamal@gmail.com>
2021-11-10 20:46:45 -0300

    Alarm

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 import hamburguerMenu from "./menu_hamburguesa.js"; //importando el modulo hamburguesa.js
-import { digitalClock } from "./reloj.js"; //Importando reloj.
+import { digitalClock,alarm } from "./reloj.js"; //Importando reloj.
 
 const d = document;  //pasamos el evento y selector.
 d.addEventListener("DOMContentLoaded", (e) => { // cargando el document.
     hamburguerMenu(".panel-btn", ".panel", ".menu a"); //envio  de parametros a funcion.
     digitalClock("#clock","#active","#stop");
+    alarm("dist/assets/sonidos/car-alarm.mp3","#enable-alarm","#disabled-alarm")
 })
 
 
--- js/reloj.js
+++ js/reloj.js
@@ -1,13 +1,13 @@
-export function digitalClock(clock, btnEnabled, btnStop) {
-    const d = document;
-    let clockTime;
+const d = document;
+let clockTime;
 
+export function digitalClock(clock, btnEnabled, btnStop) {
     d.addEventListener("click", e => {
         if (e.target.matches(btnEnabled)) {
             clockTime = setInterval(() => {
                 let hour = new Date().toLocaleTimeString(); //get hour. 
-                d.querySelector(clock).innerHTML = `<h1>${hour}</h1>`; // paint in h1
-            }, 1000); 
+                d.querySelector(clock).innerHTML = `<h1>${hour}</h1>`; // paint in container clock.
+            }, 1000);
 
             e.target.disabled = true; //disable button enabled clock
         }
@@ -19,9 +19,24 @@ export function digitalClock(clock, btnEnabled, btnStop) {
     })
 }
 
-export function alarm() {
+export function alarm(sound, btnEnabled, btnStop) {
+    let alarmanTep;
+    const $alarma = d.createElement("audio");
+    $alarma.src = sound;
+    d.addEventListener("click", e => {
+        if (e.target.matches(btnEnabled)) {
+            alarmanTep = setTimeout(() => {
+                $alarma.play();
+            }, 2000);
+            e.target.disabled = true; //disable button enabled clock
+        
+        } if (e.target.matches(btnStop)) {
+           clearTimeout(alarmanTep)
+           $alarma.pause();
+           $alarma.currentTime = 0;
+           d.querySelector(btnEnabled).disabled = false;
 
+        }
+    })
 }
-
-
 
