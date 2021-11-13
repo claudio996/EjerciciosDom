66b09ba2b2b606e24945d42e786eb5d389a304ff
claudio996 <claudioantonio.retamal@gmail.com>
2021-11-13 18:13:14 -0300

    countDowm set-interval

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 import hamburguerMenu from "./menu_hamburguesa.js"; //importando el modulo hamburguesa.js
 import { digitalClock,alarm } from "./reloj.js"; //Importando reloj.
-import {shortcuts} from  "./teclado.js"; //Importando reloj.
-
+import {moveBall, shortcuts} from  "./teclado.js"; //Importando reloj.
+import countDown from "./count_down.js";
 
 const d = document;  //pasamos el evento y selector.
 d.addEventListener("DOMContentLoaded", (e) => { // cargando el document.
     hamburguerMenu(".panel-btn", ".panel", ".menu a"); //envio  de parametros a funcion.
     digitalClock("#clock","#active","#stop");
     alarm("dist/assets/sonidos/car-alarm.mp3","#enable-alarm","#disabled-alarm")
-    
+    countDown("count-down", 'january 17 2022 12:45:45' , 'feliz cumpleaños')
 })
 
 //teclas
 d.addEventListener("keydown" , e => { // detect keys down left right  
     shortcuts(e); //recepcionamos el evento. e.wich. code key
-    
+    moveBall(e,".ball", ".stage")
 } )
--- js/teclado.js
+++ js/teclado.js
@@ -1,25 +1,67 @@
 const d = document;
+let x = 0;
+let y = 0;
 
-export const shortcuts = (e) =>{ //recibimos el evento.
-    console.log(e);
+
+export function moveBall(e, ball, stage) {
+    const $ball = d.querySelector(ball), //bola
+        $stage = d.querySelector(stage); //ecenario
+        let limitsBall;
+        let limitStage;
+
+    limitsBall = $ball.getBoundingClientRect(),
+        limitStage = $stage.getBoundingClientRect()
+
+    console.log(limitsBall, limitStage);
+
+    if (e.keyCode === 37) {
+        //move("left")
+       // e.preventDefault()
+        //definir limites del ecenario.
+        if(limitsBall.left > limitStage.left  ) x--;
+            
+    } if (e.keyCode === 38) {
+        //move("up")
+        e.preventDefault()
+        if(limitsBall.top > limitStage.top) y--;
+    
+
+    } if (e.keyCode === 39) {
+        //move("right")
+       // e.preventDefault()
+        if(limitsBall.right < limitStage.right  ) x++;
+
+    } if (e.keyCode === 40) {
+        // move("down")
+        e.preventDefault()
+        if(limitsBall.bottom < limitStage.bottom) y++;
+        //y++;
+    }
+    $ball.style.transform = `translate(${x * 10}px , ${y * 10}px)`;  //move ball
+
+
+}
+
+export const shortcuts = (e) => { //recibimos el evento.
+    /*console.log(e);
     console.log(e.type);
     console.log(e.key);
     console.log("ctl", e.ctrlKey);
-    console.log("alt",e.altKey);
+    console.log("alt", e.altKey);
     console.log("shift", e.shiftKey);
     // three types events wondow prompt alert confirm
-
+*/
     if (e.key === "a" && e.altKey) {
         alert("Has lanzado una alerta con el teclado");
-        
+
     }
     if (e.key === "c" && e.altKey) {
         confirm("Has lanzado una confirmación con el teclado");
-        
+
     }
     if (e.key === "p" && e.altKey) {
         prompt("Has lanzado un prompt con el teclado");
-        
+
     }
-   
+
 }
